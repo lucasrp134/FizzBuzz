dbddd75d652cb24647177655800624190a87ee69
Lucas Pereira <142121486+lucasrp134@users.noreply.github.com>
2024-05-14 01:02:49 -0400

    Define 'addNumber' function in main.js: Updates score box output based on FizzBuzz game logic

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const box = document.querySelector('.box');
 const button = document.querySelector('.button_');
 let number = 0;
+
+function addNumber() {
+    number += 1;
+    if (number % 3 === 0 && number % 5 === 0) {
+        box.textContent = 'FizzBuzz';
+    } else if (number % 3 === 0) {
+        box.textContent = 'Fizz';
+    } else if (number % 5 === 0) {
+        box.textContent = 'Buzz';
+    } else {
+        box.textContent = number;
+    }
+}
