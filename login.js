5399fdab254ad42ffed2481b8f2ca38dd3acec6e
Lucas Pereira <142121486+lucasrp134@users.noreply.github.com>
2024-05-15 22:49:17 -0400

    Add POST function from API

--- login.js
+++ login.js
@@ -9,3 +9,18 @@ function get(url) {
         http.send();
     });
 }
+
+//post function definition to store the username in the API
+function post(url, data) {
+    data = JSON.stringify(data);
+    return new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+        http.onload = function() {
+            resolve({ status: http.status, data: JSON.parse(http.response) });
+        };
+        http.open("POST", url);
+        //Make sure that the server knows we're sending it json data.
+        http.setRequestHeader("Content-Type", "application/json");
+        http.send(data);
+    });
+}
